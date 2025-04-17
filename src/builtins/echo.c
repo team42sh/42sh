b597da204dd0b0703c44b64c61105962daba3895
Antoine MALGAT <antoine.malgat@epitech.eu>
2025-04-17 10:38:12 +0200

    [+] corrected the echo command that did not escaped backslash character correctly

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -11,6 +11,64 @@
 #include <stddef.h>
 
 /**
+ * @brief change the backslash to the real escaped character, part 2
+ * @param arg the arg to print
+ * @param offset the offset caused by other backslash
+ * @return char* the arg changed
+ */
+static char *put_backslash_two(IN char *arg, OUT int *offset)
+{
+    switch (*arg) {
+        case 'r':
+            arg[- *offset] = '\r';
+            return arg;
+        case 't':
+            arg[- *offset] = '\t';
+            return arg;
+        case 'v':
+            arg[- *offset] = '\v';
+            return arg;
+        case '\\':
+            arg[- *offset] = '\\';
+            return arg;
+        default:
+            (*offset)--;
+            arg--;
+            return arg;
+    }
+}
+
+/**
+ * @brief change the backslash to the real escaped character
+ *
+ * @param arg the arg to print
+ * @param offset the offset caused by other backslash
+ * @return char* the arg changed
+ */
+static char *put_backslash(IN char *arg, OUT int *offset)
+{
+    switch (*arg) {
+        case 'a':
+            arg[- *offset] = '\a';
+            return arg;
+        case 'b':
+            arg[- *offset] = '\b';
+            return arg;
+        case 'e':
+            arg[- *offset] = '\e';
+            return arg;
+        case 'f':
+            arg[- *offset] = '\f';
+            return arg;
+        case 'n':
+            arg[- *offset] = '\n';
+            return arg;
+        default:
+            return put_backslash_two(arg, offset);
+    }
+}
+
+/**
  * @brief replace the backslash with their coresponding ascii character
  *
  * @param arg the argument
@@ -18,25 +76,20 @@
  */
 static char *replace_backslash(IN char *arg)
 {
-    char *tmp;
+    int offset = 0;
+    char *ptr = arg;
 
-    tmp = my_strreplace(arg, "\\n", "\n");
-    free(arg);
-    arg = my_strreplace(tmp, "\\a", "\a");
-    free(tmp);
-    tmp = my_strreplace(arg, "\\e", "\e");
-    free(arg);
-    arg = my_strreplace(tmp, "\\f", "\f");
-    free(tmp);
-    tmp = my_strreplace(arg, "\\r", "\r");
-    free(arg);
-    arg = my_strreplace(tmp, "\\t", "\t");
-    free(tmp);
-    tmp = my_strreplace(arg, "\\v", "\v");
-    free(arg);
-    arg = my_strreplace(tmp, "\\\\", "\\");
-    free(tmp);
-    return arg;
+    while (*arg != '\0') {
+        if (*arg == '\\') {
+            arg++;
+            offset++;
+            arg = put_backslash(arg, &offset);
+        } else
+            arg[- offset] = *arg;
+        arg++;
+    }
+    arg[- offset] = '\0';
+    return ptr;
 }
 
 /**
