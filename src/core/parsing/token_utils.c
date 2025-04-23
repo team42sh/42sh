221eab57c6c9e23e6f1d293d0562209d0a4d13dd
matteoepitech <matteo.tutti@epitech.eu>
2025-04-23 09:42:30 +0200

    [~] Fix some errors handling about some commands with a wrong syntax

--- src/core/parsing/token_utils.c
+++ src/core/parsing/token_utils.c
@@ -7,7 +7,7 @@
 
 #include "core/minishell.h"
 
-bool is_token_redirection_append(token_t *token)
+bool is_token_redirection_append(IN token_t *token)
 {
     if (token == NULL)
         return false;
@@ -17,12 +17,32 @@ bool is_token_redirection_append(token_t *token)
     return false;
 }
 
-bool is_token_redirection_simple(token_t *token)
+bool is_token_redirection_simple(IN token_t *token)
 {
     if (token == NULL)
         return false;
     if (token->token_type == TOKEN_LEFT_REDIRECTION ||
         token->token_type == TOKEN_RIGHT_REDIRECTION)
+        return true;
+    return false;
+}
+
+bool is_token_redirection_right(IN token_t *token)
+{
+    if (token == NULL)
+        return false;
+    if (token->token_type == TOKEN_RIGHT_REDIRECTION ||
+        token->token_type == TOKEN_RIGHT_APPEND)
+        return true;
+    return false;
+}
+
+bool is_token_redirection_left(IN token_t *token)
+{
+    if (token == NULL)
+        return false;
+    if (token->token_type == TOKEN_LEFT_REDIRECTION ||
+        token->token_type == TOKEN_LEFT_APPEND)
         return true;
     return false;
 }
