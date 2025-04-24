fefb205bb4012545accbdb340c230b20d081e587
Del <matteo.tutti@epitech.eu>
2025-04-24 14:35:54 +0200

    [~] Fix errors handling about redirections and pipes. (#39)
    * [~] Fix some errors handling about some commands with a wrong syntax
    
    * [~] Adding comments in prototypes functions
    
    * [~] fixed file being removed by other tests
    
    * [~] fixed error while printing the number of test failed
    
    * [+] fixed minishell two test that would be correct 50% of the time
    
    * [+] fixed error on redirection that made new child that didn't exited
    
    * [~] Did some coding style, especially in the comments of some functions being too long.
    
    ---------
    
    Co-authored-by: Antoine MALGAT <antoine.malgat@epitech.eu>

--- src/core/parsing/token_utils.c
+++ src/core/parsing/token_utils.c
@@ -7,7 +7,14 @@
 
 #include "core/minishell.h"
 
-bool is_token_redirection_append(token_t *token)
+/**
+ * @brief Is token, a redirection append.
+ *
+ * @param token         The token
+ *
+ * @return TRUE or FALSE.
+ */
+bool is_token_redirection_append(IN token_t *token)
 {
     if (token == NULL)
         return false;
@@ -17,12 +24,53 @@ bool is_token_redirection_append(token_t *token)
     return false;
 }
 
-bool is_token_redirection_simple(token_t *token)
+/**
+ * @brief Is token, a redirection simple.
+ *
+ * @param token         The token
+ *
+ * @return TRUE or FALSE.
+ */
+bool is_token_redirection_simple(IN token_t *token)
 {
     if (token == NULL)
         return false;
     if (token->token_type == TOKEN_LEFT_REDIRECTION ||
         token->token_type == TOKEN_RIGHT_REDIRECTION)
+        return true;
+    return false;
+}
+
+/**
+ * @brief Is token, a redirection right.
+ *
+ * @param token         The token
+ *
+ * @return TRUE or FALSE.
+ */
+bool is_token_redirection_right(IN token_t *token)
+{
+    if (token == NULL)
+        return false;
+    if (token->token_type == TOKEN_RIGHT_REDIRECTION ||
+        token->token_type == TOKEN_RIGHT_APPEND)
+        return true;
+    return false;
+}
+
+/**
+ * @brief Is token, a redirection left.
+ *
+ * @param token         The token
+ *
+ * @return TRUE or FALSE.
+ */
+bool is_token_redirection_left(IN token_t *token)
+{
+    if (token == NULL)
+        return false;
+    if (token->token_type == TOKEN_LEFT_REDIRECTION ||
+        token->token_type == TOKEN_LEFT_APPEND)
         return true;
     return false;
 }
