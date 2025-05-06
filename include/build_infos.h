2300b83aee22151ec9e30643ab904891c06b6c67
Neptnium <quentin.ritzler@epitech.eu>
2025-05-06 15:37:10 +0200

    [+] Add build_info header and all tasks for it (#64)

---
+++ include/build_infos.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** 42sh-ci
+** File description:
+** build_infos
+*/
+
+#ifndef BUILD_INFOS_H_
+    #define BUILD_INFOS_H_
+
+    #define VERSION "UNKWN"
+    #define ALLOW_AUTO_UPDATE 0
+    #define SHOW_UPDATES 0
+
+#endif /* !BUILD_INFOS_H_ */
