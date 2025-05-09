85b2e2faf708de10e3990df34333bfb93b8f5918
Neptnium <q.ritzler@gmail.com>
2025-05-09 14:21:02 +0200

    [~] Set correct download url for auto update

--- include/update_infos.h
+++ include/update_infos.h
@@ -11,10 +11,10 @@
     #define GIT_URL "https://github.com"
     #define GIT_API_URL "https://api.github.com"
 
-    #define UPDATE_VERSION_URL_PATH "repos/team42sh/42sh-ci/releases/latest"
+    #define UPDATE_VERSION_URL_PATH "repos/team42sh/42sh/releases/latest"
     #define UPDATE_VERSION_URL GIT_API_URL "/" UPDATE_VERSION_URL_PATH
 
-    #define DOWNLOAD_URL_BASE GIT_URL "/team42sh/42sh-ci/releases/download"
+    #define DOWNLOAD_URL_BASE GIT_URL "/team42sh/42sh/releases/download"
     #define DOWNLOAD_PRG_BASE_NAME "42sh"
 
 #endif /* !UPDATE_INFOS_H_ */
