49e8464659c1cf726289391caf17093cfec88638
Neptnium <q.ritzler@gmail.com>
2025-05-09 14:20:25 +0200

    [~] Fix build infos file

--- include/build_infos.h
+++ include/build_infos.h
@@ -8,9 +8,9 @@
 #ifndef BUILD_INFOS_H_
     #define BUILD_INFOS_H_
 
-    #define VERSION "v0.0.2"
-    #define ALLOW_AUTO_UPDATE 1
-    #define SHOW_UPDATES 1
-    #define OS_VER "macos.arm64"
+    #define VERSION "UNKWN"
+    #define ALLOW_AUTO_UPDATE 0
+    #define SHOW_UPDATES 0
+    #define OS_VER "UNKWN"
 
 #endif /* !BUILD_INFOS_H_ */
